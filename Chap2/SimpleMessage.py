993d057f0dec0503435234fecdfdd0cb7d2a3bfd
Mark <mark@dev.haus>
2017-02-11 20:38:45 +0900

    2.1 and 2.2

---
+++ Chap2/SimpleMessage.py
@@ -0,0 +1,2 @@
+a_simple_message = "I like big butts and I cannot lie!"
+print(a_simple_message)
---
+++ Chap2/SimpleMessages.py
@@ -0,0 +1,4 @@
+message1 = "This is message one"
+print(message1)
+message1 = "Changed to this one!"
+print(message1)
