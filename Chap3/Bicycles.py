1b50c09a1829fd8d23570650d1328655955aed17
Mark <mark@dev.haus>
2017-02-18 17:28:38 +0900

    Chap 3 - Bicycles done

--- Chap3/Bicycles.py
+++ Chap3/Bicycles.py
@@ -1,8 +1,6 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-print(bicycles)
 
-print(bicycles[0].title())
-
-print(bicycles[-1].title())
+message = "My first bike was a " + bicycles[1].title() + "."
+print(message)
 
 
