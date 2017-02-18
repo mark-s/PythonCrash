fb30945e067842de163e64c0e4069093542a9921
Mark <mark@dev.haus>
2017-02-18 18:18:04 +0900

    Chap 3 - lists

---
+++ Chap3/Greetings.py
@@ -0,0 +1,7 @@
+names = ['bob', 'bill', 'sarah', 'zara']
+greeting = "Hello, "
+
+print(greeting + names[0].title())
+print(greeting + names[1].title())
+print(greeting + names[2].title())
+print(greeting + names[3].title())
---
+++ Chap3/Motorcycles.py
@@ -0,0 +1,22 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+# change first item
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+
+# add one
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
+
+# insert
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+
