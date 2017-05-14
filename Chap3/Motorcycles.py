c0985b82cef2d8b588b285d177d3db4073c347ed
Mark <mark@dev.haus>
2017-05-14 23:47:53 +0900

    Motercycles done!

--- Chap3/Motorcycles.py
+++ Chap3/Motorcycles.py
@@ -20,3 +20,23 @@ print(motorcycles)
 motorcycles.insert(0, 'ducati')
 print(motorcycles)
 
+
+# remove
+del motorcycles[0]
+print(motorcycles)
+
+# pop
+print("pop")
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+
+
+# remove
+motorcycles = ['honda', 'yamaha', 'suzuki']
+too_expensive = 'yamaha'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+
